4585bec8999ac382252acb0aea0ec355dfa70771
Torjus Eidet <torjue@gmail.com>
2013-07-08 23:50:19 +0200

    Some tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+
 		pkg: grunt.file.readJSON('package.json'),
+
 		jshint: {
 			gruntfile: {
 				src: 'Gruntfile.js',
@@ -17,6 +19,7 @@ module.exports = function(grunt) {
 				]
 			}
 		},
+
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
@@ -29,21 +32,28 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		concat: {
 			options: {
-				stripBanners: true,
 				banner: '<%= uglify.options.banner %>'
 			},
-			dist: {
+			build: {
 				src: ['src/subsumer.js'],
 				dest: 'build/subsumer.js'
 			}
+		},
+
+		nodeunit: {
+			all: ['test/*_test.js']
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'concat']);
+	grunt.registerTask('default', ['jshint', 'nodeunit']);
+	grunt.registerTask('build', ['jshint', 'nodeunit', 'uglify', 'concat']);
 };
